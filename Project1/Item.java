454a96b9e4815c0a4bd6024bce5a6580a575ae2e
E3-101 <tanop.udo@student.mahidol.edu>
2025-09-20 21:12:24 +0700

    + Item01

---
+++ Project1/Item.java
@@ -0,0 +1,50 @@
+
+package Project1;
+
+interface Room{
+    double totalRoom();
+    void printRoom();
+    double calculateRoom(String rooms, double days);
+}
+interface Meal{
+    void printMeal();
+    double calculateMeal(String meals, double unit);
+}
+
+public class Item implements Room, Meal{
+    
+    String code, name;
+    double price;
+    
+    public Item(String c, String n, double p){
+        code  = c;
+        name  = n;
+        price = p;
+    }
+    @Override   
+    public double totalRoom(){
+        price = price*(1.1)*(1.07);
+        return price;
+    }
+    @Override
+    public void printRoom(){
+        System.out.printf("%S, %-22s rate (per day) = %,9.2f    ",code,name,price);
+        totalRoom();
+        System.out.printf("rate++ = %,9.2f\n",price);
+    }
+    @Override
+    public double calculateRoom(String rooms, double days){
+        return price;
+    }
+    @Override
+    public void printMeal(){
+        System.out.printf("%S, %-15s rate (per person per day) = %6.2f\n",code,name,price);
+    }
+    @Override
+    public double calculateMeal(String meals, double unit){
+        return price;
+    }
+}
+
+
+
---
+++ Project1/ItemMain.java
@@ -0,0 +1,46 @@
+
+package Project1;
+
+import java.util.*;
+import java.io.*;
+
+public class ItemMain {
+
+    public static void main(String[] args) {
+        
+        readFrom("items.txt");
+        
+    }
+    
+    static void readFrom(String filename){
+
+        //String path = "src/main/java/Project1/";
+        String path = "src/Project1/";
+        File file = new File(path+filename);
+        System.out.println("Read from "+path+filename);
+        try ( Scanner iscan = new Scanner(file) ) {
+            List<Item> items = new ArrayList<>();
+            iscan.nextLine();                                   //skip first line
+            while(iscan.hasNext()) {
+                String line = iscan.nextLine(); 
+                String []cols = line.split(","); 
+                String code = cols[0].trim();
+                String name = cols[1].trim();
+                double price = Double.parseDouble( cols[2].trim() );
+                Item item = new Item(code,name,price);
+                items.add(item);
+            }
+        for (int i=0;i<3;i++){
+            Item item = items.get(i);
+            item.printRoom();
+        } System.out.println();   
+        for (int i=3;i<6;i++){
+            Item item = items.get(i);
+            item.printMeal();
+        } System.out.println();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
